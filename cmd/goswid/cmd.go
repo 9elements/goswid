950d50fb915fa0382828613f49fdbb7cc2f70dd1
Maximilian Brune <maximilian.brune@9elements.com>
2022-05-17 15:30:22 +0200

    Add print command to print SWID to stdout

--- cmd/goswid/cmd.go
+++ cmd/goswid/cmd.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"bytes"
+	"encoding/json"
 
 	"github.com/9elements/goswid/pkg/uswid"
 	"github.com/google/uuid"
@@ -23,6 +25,7 @@ var cli struct {
 	Debug         bool               `help:"Enable debug mode."`
 
 	GenerateTagID generateTagIDCmd   `cmd help:"generates a 16 byte type-5 SHA1 RFC 4122 UUID (possible use for tag-id)"`
+	Print         printCmd           `cmd help:"print swid tag to stdout (in json format)"`
 	Convert       convertCmd         `cmd help:"convert between SWID/CoSWID and different file formats (json, xml, cbor, uswid)"`
 }
 
@@ -34,6 +37,10 @@ type convertCmd struct {
 
 type generateTagIDCmd struct {
 	UuidgenName string   `flag required short:"n" name:"name" help:"string to use for uuid generation (e.g. software name)"`
+}
+
+type printCmd struct {
+	InputFiles []string `arg required name:"input-file-paths" help:"Paths to imput files." type:"existingfile"`
 }
 
 func (c *convertCmd) Run() error {
@@ -100,6 +107,46 @@ func (c *convertCmd) Run() error {
 	if err := ioutil.WriteFile(c.OutputFile, output_buf, 0644); err != nil {
 		return err
 	}
+	return nil
+}
+
+func (p *printCmd) Run() error {
+	/* check file extension of output file */
+	var uswid_input_tag uswid.UswidSoftwareIdentity
+	for _, input_file_path := range p.InputFiles {
+		input_file, err := ioutil.ReadFile(input_file_path)
+		if err != nil {
+			return err
+		}
+
+		/* check file extension of input file */
+		if_parts := strings.Split(input_file_path, ".")
+		switch if_parts[len(if_parts)-1] {
+		case "json":
+			err = uswid_input_tag.FromJSON(input_file)
+		case "xml":
+			err = uswid_input_tag.FromXML(input_file)
+		case "cbor":
+			err = uswid_input_tag.FromCBOR(input_file, false)
+		case "uswid":
+			fallthrough
+		default:
+			_, err = uswid_input_tag.FromUSWID(input_file)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	output_buf, err := uswid_input_tag.ToJSON()
+	if err != nil {
+		return err
+	}
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, []byte(output_buf), "", "    "); err != nil {
+		return err
+	}
+
+	fmt.Println(prettyJSON.String())
 	return nil
 }
 
