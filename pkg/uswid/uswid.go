bdd55e42029b8ef734abfd56efe789d41996bfd4
Maximilian Brune <maximilian.brune@9elements.com>
2022-08-11 13:59:07 +0200

    Add json minify to remove comments in JSON files
    there is a common desire to put comments in a JSON files.
    For example a license header at the top of the JSON file or
    some other comments for clarfication of the contents.
    Since comments is not supported in pure JSON, we need to remove
    them before giving them to any kind of parser

--- pkg/uswid/uswid.go
+++ pkg/uswid/uswid.go
@@ -25,6 +25,94 @@ type UswidSoftwareIdentity struct {
 	Identities []swid.SoftwareIdentity
 }
 
+// strip comments from JSON to get pure JSON. optionally remove whitespaces, tabs and newlines
+func jsonMinify(json string, stripSpace bool) string {
+	var out strings.Builder
+	indexMultilineComment := -1
+	indexInlineComment := -1
+	inJsonStr := false
+	skip := false
+	for i, r := range json {
+		if skip {
+			skip = false
+			continue
+		}
+		switch r {
+		case '/':
+			if !inJsonStr && indexMultilineComment == -1 && indexInlineComment == -1 {
+				// if currently not in a comment or json string, a slash
+				// could be the beginning of a new comment. look ahead and see
+				if len(json) > i+1 {
+					if json[i+1] == '/' {
+						indexInlineComment = i
+						// since we already looked ahead, we don't need
+						// to consider the next character
+						skip = true
+					} else if json[i+1] == '*' {
+						indexMultilineComment = i
+						// since we already looked ahead, we don't need
+						// to consider the next character
+						skip = true
+					}
+				}
+			}
+		case '*':
+			if indexMultilineComment != -1 {
+				// we are currently in a multiline comment. A star sign could mean
+				// the end of our multiline comment. look ahead and see
+				if len(json) > i+1 {
+					if json[i+1] == '/' {
+						indexMultilineComment = -1
+						// since we already looked ahead, we don't need to consider
+						// the next character
+						skip = true
+						// a bit nasty but since we don't want to print the current
+						// character, we need to skip the rest
+						continue
+					}
+				}
+			}
+		case '"':
+			if inJsonStr {
+				inJsonStr = false
+			} else if !inJsonStr && indexMultilineComment == -1 && indexInlineComment == -1 {
+				// if we are currently not in a comment, a quote sign can only mean
+				// we got into a JSON string
+				inJsonStr = true
+			}
+		case '\\':
+			if len(json) > i+1 {
+				if json[i+1] == '"' {
+					// next quote sign is escaped and therefore looses it's meaning
+					// as start/end of JSON string. put into output and skip both
+					out.WriteRune('\\')
+					out.WriteRune('"')
+					skip = true
+					continue
+				} else if json[i+1] == '\\' {
+					// the next escape must loose it's power to escape stuff
+					// because it itself is escaped
+					out.WriteRune('\\')
+					skip = true
+				}
+			}
+		case '\n':
+			indexInlineComment = -1
+		case '\r':
+			indexInlineComment = -1
+		}
+		if indexMultilineComment == -1 && indexInlineComment == -1 {
+			// only consider in output if character is outside of any comment
+			if !stripSpace || inJsonStr || (r != '\n' && r != '\r' && r != '\t' && r != ' ') {
+				// everything considered a whitespace (line feed, carriage return, tab, whitespace)
+				// is only candidate for output if desired
+				out.WriteRune(r)
+			}
+		}
+	}
+	return out.String()
+}
+
 func (uswid *UswidSoftwareIdentity) FromFile(filepath string) error {
 	inputFile, err := ioutil.ReadFile(filepath)
 	if err != nil {
@@ -38,7 +126,8 @@ func (uswid *UswidSoftwareIdentity) FromFile(filepath string) error {
 		pcStr := strings.ReplaceAll(string(inputFile), "\r\n", "\n") // replace windows line endings with line feeds
 		err = uswid.FromPC(pcStr, filepath)
 	case "json":
-		err = uswid.FromJSON(string(inputFile))
+		jsonStr := jsonMinify(string(inputFile), false)
+		err = uswid.FromJSON(jsonStr)
 	case "xml":
 		err = uswid.FromXML(string(inputFile))
 	case "cbor":
