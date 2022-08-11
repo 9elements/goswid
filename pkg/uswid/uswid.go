e86b1d1bf6052ebcf75d6267b9550e58646c60c9
Maximilian Brune <maximilian.brune@9elements.com>
2022-08-11 13:41:08 +0200

    Fix up variables for camelCase scheme

--- pkg/uswid/uswid.go
+++ pkg/uswid/uswid.go
@@ -26,27 +26,27 @@ type UswidSoftwareIdentity struct {
 }
 
 func (uswid *UswidSoftwareIdentity) FromFile(filepath string) error {
-	input_file, err := ioutil.ReadFile(filepath)
+	inputFile, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		return err
 	}
 
 	/* check file extension of input file */
-	if_parts := strings.Split(filepath, ".")
-	switch if_parts[len(if_parts)-1] {
+	ifParts := strings.Split(filepath, ".")
+	switch ifParts[len(ifParts)-1] {
 	case "pc":
-		pc_str := strings.ReplaceAll(string(input_file), "\r\n", "\n") // replace windows line endings with line feeds
-		err = uswid.FromPC(pc_str, filepath)
+		pcStr := strings.ReplaceAll(string(inputFile), "\r\n", "\n") // replace windows line endings with line feeds
+		err = uswid.FromPC(pcStr, filepath)
 	case "json":
-		err = uswid.FromJSON(input_file)
+		err = uswid.FromJSON(string(inputFile))
 	case "xml":
-		err = uswid.FromXML(input_file)
+		err = uswid.FromXML(string(inputFile))
 	case "cbor":
-		err = uswid.FromCBOR(input_file, false)
+		err = uswid.FromCBOR(inputFile, false)
 	case "uswid":
 		fallthrough
 	default:
-		_, err = uswid.FromUSWID(input_file)
+		_, err = uswid.FromUSWID(inputFile)
 	}
 	if err != nil {
 		return fmt.Errorf("parsing %s: %w", filepath, err)
@@ -87,17 +87,17 @@ func (uswid *UswidSoftwareIdentity) FromUSWID(blob []byte) (int, error) {
 	if offset == -1 {
 		return -1, errors.New("could not find uswid data")
 	}
-	header_version := blob[offset+16]
-	if header_version != 2 {
+	headerVersion := blob[offset+16]
+	if headerVersion != 2 {
 		return offset, errors.New("no known header version")
 	}
 	//header_len := binary.LittleEndian.Uint16(blob[offset+17:offset+19])
-	payload_len := binary.LittleEndian.Uint32(blob[offset+19 : offset+23])
+	payloadLen := binary.LittleEndian.Uint32(blob[offset+19 : offset+23])
 	flags := blob[offset+23]
 	if (flags & flagCompressZlib) != 0 {
-		err = uswid.FromCBOR(blob[offset+24:uint32(offset)+24+payload_len], true)
+		err = uswid.FromCBOR(blob[offset+24:uint32(offset)+24+payloadLen], true)
 	} else {
-		err = uswid.FromCBOR(blob[offset+24:uint32(offset)+24+payload_len], false)
+		err = uswid.FromCBOR(blob[offset+24:uint32(offset)+24+payloadLen], false)
 	}
 	if err != nil {
 		return -1, fmt.Errorf("extract CBOR: %w", err)
@@ -105,22 +105,22 @@ func (uswid *UswidSoftwareIdentity) FromUSWID(blob []byte) (int, error) {
 	return offset, nil
 }
 
-func (uswid *UswidSoftwareIdentity) FromJSON(json_data []byte) error {
-	if len(json_data) == 0 {
+func (uswid *UswidSoftwareIdentity) FromJSON(jsonStr string) error {
+	if len(jsonStr) == 0 {
 		return errors.New("input data empty")
 	}
 
-	json_data = []byte(strings.TrimSpace(string(json_data)))
+	jsonStr = strings.TrimSpace(jsonStr)
 
-	if json_data[0] == '[' && json_data[len(json_data)-1] == ']' {
-		var uswid_id UswidSoftwareIdentity
-		if err := json.Unmarshal(json_data, &uswid_id.Identities); err != nil {
+	if jsonStr[0] == '[' && jsonStr[len(jsonStr)-1] == ']' {
+		var uswidID UswidSoftwareIdentity
+		if err := json.Unmarshal([]byte(jsonStr), &uswidID.Identities); err != nil {
 			return err
 		}
-		uswid.Identities = append(uswid.Identities, uswid_id.Identities...)
+		uswid.Identities = append(uswid.Identities, uswidID.Identities...)
 	} else {
 		var id swid.SoftwareIdentity
-		if err := id.FromJSON(json_data); err != nil {
+		if err := id.FromJSON([]byte(jsonStr)); err != nil {
 			return err
 		}
 		uswid.Identities = append(uswid.Identities, id)
@@ -128,33 +128,33 @@ func (uswid *UswidSoftwareIdentity) FromJSON(json_data []byte) error {
 	return nil
 }
 
-func (uswid *UswidSoftwareIdentity) FromXML(xml_data []byte) error {
-	if len(xml_data) == 0 {
+func (uswid *UswidSoftwareIdentity) FromXML(xmlStr string) error {
+	if len(xmlStr) == 0 {
 		return errors.New("input data empty")
 	}
 
 	var offset int64 = 0
-	for offset < int64(len(xml_data)) {
+	for offset < int64(len(xmlStr)) {
 		var id swid.SoftwareIdentity
-		xml_decoder := xml.NewDecoder(bytes.NewReader(xml_data[offset:]))
-		if err := xml_decoder.Decode(&id); err != nil {
+		xmlDecoder := xml.NewDecoder(bytes.NewReader([]byte(xmlStr[offset:])))
+		if err := xmlDecoder.Decode(&id); err != nil {
 			if err == io.EOF {
 				break
 			}
 			return err
 		}
 		uswid.Identities = append(uswid.Identities, id)
-		offset += xml_decoder.InputOffset()
+		offset += xmlDecoder.InputOffset()
 	}
 	return nil
 }
 
-func (uswid *UswidSoftwareIdentity) FromPC(pc_data string, filename string) error {
-	pc_lines := strings.Split(pc_data, "\n")
+func (uswid *UswidSoftwareIdentity) FromPC(pcData string, filename string) error {
+	pcLines := strings.Split(pcData, "\n")
 	var id swid.SoftwareIdentity
-	var software_meta swid.SoftwareMeta
-	for _, pc_line := range pc_lines {
-		field := strings.Split(pc_line, ":")
+	var softwareMeta swid.SoftwareMeta
+	for _, pcLine := range pcLines {
+		field := strings.Split(pcLine, ":")
 		if (len(field) != 2) {
 			continue
 		}
@@ -163,12 +163,12 @@ func (uswid *UswidSoftwareIdentity) FromPC(pc_data string, filename string) erro
 		case "Name":
 			id.SoftwareName = value
 		case "Description":
-			software_meta.Summary = value
+			softwareMeta.Summary = value
 		case "Version":
 			id.SoftwareVersion = value
 		}
 	}
-	id.AddSoftwareMeta(software_meta)
+	id.AddSoftwareMeta(softwareMeta)
 	id.TagID = *swid.NewTagID(uuid.NewSHA1(uuid.NameSpaceDNS, []byte(filename)))
 	if len(id.Entities) == 0 {
 		entity, _ := swid.NewEntity("goswid (auto-generated)", swid.RoleTagCreator)
@@ -188,39 +188,39 @@ func (uswid UswidSoftwareIdentity) ToUSWID(compress bool) ([]byte, error) {
 	if compress {
 		header[23] |= 0x01
 	}
-	cbor_buf, err := uswid.ToCBOR(compress)
+	cborBuf, err := uswid.ToCBOR(compress)
 	if err != nil {
 		return nil, err
 	}
-	binary.LittleEndian.PutUint32(header[19:23], uint32(len(cbor_buf)))
-	return append(header[:], cbor_buf...), nil
+	binary.LittleEndian.PutUint32(header[19:23], uint32(len(cborBuf)))
+	return append(header[:], cborBuf...), nil
 }
 
 func (uswid UswidSoftwareIdentity) ToJSON() ([]byte, error) {
-	var json_buf []byte
+	var jsonBuf []byte
 	if len(uswid.Identities) > 1 {
-		json_buf = append(json_buf, '[')
-		json_buf = append(json_buf, ' ')
+		jsonBuf = append(jsonBuf, '[')
+		jsonBuf = append(jsonBuf, ' ')
 	}
 	for i, id := range uswid.Identities {
 		buf, err := id.ToJSON()
 		if err != nil {
 			return nil, fmt.Errorf("convert to JSON: %w", err)
 		}
-		json_buf = append(json_buf, buf...)
+		jsonBuf = append(jsonBuf, buf...)
 		if i < len(uswid.Identities)-1 {
-			json_buf = append(json_buf, ',')
+			jsonBuf = append(jsonBuf, ',')
 		}
 	}
 	if len(uswid.Identities) > 1 {
-		json_buf = append(json_buf, ' ')
-		json_buf = append(json_buf, ']')
+		jsonBuf = append(jsonBuf, ' ')
+		jsonBuf = append(jsonBuf, ']')
 	}
-	return json_buf, nil
+	return jsonBuf, nil
 }
 
 func (uswid UswidSoftwareIdentity) ToXML() ([]byte, error) {
-	var xml_buf []byte
+	var xmlBuf []byte
 	for _, id := range uswid.Identities {
 		id.XMLName.Space = "http://standards.iso.org/iso/19770/-2/2015/schema.xsd"
 		id.XMLName.Local = "SoftwareIdentity"
@@ -229,30 +229,30 @@ func (uswid UswidSoftwareIdentity) ToXML() ([]byte, error) {
 		if err != nil {
 			return nil, fmt.Errorf("convert to XML: %w", err)
 		}
-		xml_buf = append(xml_buf, buf...)
+		xmlBuf = append(xmlBuf, buf...)
 	}
-	return xml_buf, nil
+	return xmlBuf, nil
 }
 
 func (uswid UswidSoftwareIdentity) ToCBOR(compress bool) ([]byte, error) {
-	var cbor_buf []byte
+	var cborBuf []byte
 	for _, id := range uswid.Identities {
 		buf, err := id.ToCBOR()
 		if err != nil {
 			return nil, fmt.Errorf("convert to CBOR: %w", err)
 		}
-		cbor_buf = append(cbor_buf, buf...)
+		cborBuf = append(cborBuf, buf...)
 	}
 	if compress {
 		var buf bytes.Buffer
-		zlib_writer := zlib.NewWriter(&buf)
-		_, err := zlib_writer.Write(cbor_buf)
+		zlibWriter := zlib.NewWriter(&buf)
+		_, err := zlibWriter.Write(cborBuf)
 		if err != nil {
 			return nil, fmt.Errorf("cannot zlib compress CBOR data: %w", err)
 		}
-		zlib_writer.Close()
+		zlibWriter.Close()
 		return buf.Bytes(), nil
 	} else {
-		return cbor_buf, nil
+		return cborBuf, nil
 	}
 }
